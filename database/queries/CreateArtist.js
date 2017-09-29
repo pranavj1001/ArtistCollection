1f51a835d8d568f4ef6ca123bbe6d5775ddf1643
pranavj1001 <pranavj1001@gmail.com>
2017-09-29 22:11:39 +0530

    add createArtist query

--- database/queries/CreateArtist.js
+++ database/queries/CreateArtist.js
@@ -6,4 +6,18 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the Artist that was created
  */
 module.exports = (artistProps) => {
+
+  // const artist = new Artist({
+  //   name: artistProps.name,
+  //   age: artistProps.age,
+  //   yearsActive: artistProps.yearsActive,
+  //   genre: artistProps.genre
+  // });
+
+  // or
+
+  const artist = new Artist(artistProps);
+
+  return artist.save();
+
 };
