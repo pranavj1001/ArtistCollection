d3a89fe547c9e712347991e8b700fa0c0a6498f7
pranavj1001 <pranavj1001@gmail.com>
2017-09-30 14:32:38 +0530

    add SearchArtist query

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -7,6 +7,27 @@ const Artist = require('../models/artist');
  * @param {integer} offset How many records to skip in the result set
  * @param {integer} limit How many records to return in the result set
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
+ * like this: { all: [artists], count: count, offset: offset, limit: limit };
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+
+  // ES5 approach
+  // const sortOrder = {};
+  // sortOrder[sortProperty] = 1;
+
+  const getListOfArtists = Artist
+    .find({})
+    .sort({ [sortProperty]: 1 }) // at run time, look at sortProperty and add 1
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([getListOfArtists, Artist.count()])
+    .then((results) => {
+      return {
+        all: results[0],
+        count: results[1],
+        offset,
+        limit
+      };
+    });
 };
